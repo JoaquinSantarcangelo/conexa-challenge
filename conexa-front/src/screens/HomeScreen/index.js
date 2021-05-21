6fca145f8856ed754f714204d75c1c593840f6ea
Joaquin Santarcangelo <joaquinsantarcangelo@gmail.com>
2021-05-21 09:42:06 -0300

    Add homescreen inner router

--- conexa-front/src/screens/HomeScreen/index.js
+++ conexa-front/src/screens/HomeScreen/index.js
@@ -1,8 +1,23 @@
 import React from "react";
 import "./index.sass";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+//Screens
+import PhotosScreen from "./PhotosScreen";
+import PostsScreen from "./PostsScreen";
 
 const HomeScreen = () => {
-  return <div>HomeScreen</div>;
+  return (
+    <div>
+      HomeScreen
+      <Router>
+        <Switch>
+          <Route path="/posts" exact component={PostsScreen}></Route>
+          <Route path="/photos" component={PhotosScreen}></Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 };
 
 export default HomeScreen;
