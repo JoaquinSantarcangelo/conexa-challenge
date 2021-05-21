3826f225dfb2b1d91e394a40ed71604dc09e00e8
Joaquin Santarcangelo <joaquinsantarcangelo@gmail.com>
2021-05-21 09:50:40 -0300

    Imported sass files to App.js

--- conexa-front/src/App.js
+++ conexa-front/src/App.js
@@ -2,6 +2,14 @@ import React from "react";
 import { Counter } from "./features/counter/Counter";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
+// -- Styles -- //
+import "./styles/index.sass";
+import "./styles/globalStyles.sass";
+
+//  Screens Styles //
+import "./screens/LoginScreen/index.sass";
+import "./screens/HomeScreen/index.sass";
+
 //Screens
 import LoginScreen from "./screens/LoginScreen";
 import HomeScreen from "./screens/HomeScreen";
