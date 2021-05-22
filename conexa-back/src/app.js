92610f0255ce35e65b22285c2b06ebfdae5ef670
Joaquin Santarcangelo <joaquinsantarcangelo@gmail.com>
2021-05-22 14:01:27 -0300

    Updated dependencies

--- conexa-back/src/app.js
+++ conexa-back/src/app.js
@@ -1,4 +1,9 @@
-const express = require("express");
+import express from "express";
+import morgan from "morgan";
+
 const app = express();
+
+//Middelwares
+app.use(morgan("dev"));
 
 export default app;
