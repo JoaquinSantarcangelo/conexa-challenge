0c7ae489c02fba063ecfb9d5f087b8b0dbcfee89
Joaquin Santarcangelo <joaquinsantarcangelo@gmail.com>
2021-05-23 15:20:15 -0300

    getPosts asyncthunk service

---
+++ conexa-front/src/services/user.service.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+//Consts
+import { API_URL } from "../consts";
+
+class UserService {
+  getPosts() {
+    return createAsyncThunk(
+      "post/getPosts",
+      async () => await axios.get(`${API_URL}/posts`)
+    );
+  }
+
+  getPhotos() {}
+}
+
+export default new UserService();
