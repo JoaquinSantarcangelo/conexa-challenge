0a612ad573be229a8ba70169ae33b54430a3d527
Joaquin Santarcangelo <joaquinsantarcangelo@gmail.com>
2021-05-23 10:52:50 -0300

    signUp controller working

--- conexa-back/src/controllers/auth.controller.js
+++ conexa-back/src/controllers/auth.controller.js
@@ -2,6 +2,24 @@ import User from "../models/User";
 import jwt from "jsonwebtoken";
 import config from "../config";
 
+export const signUp = async (req, res) => {
+  const { email, password, username } = req.body;
+
+  const newUser = new User({
+    username,
+    email,
+    password: await User.encryptPassword(password),
+  });
+
+  const savedUser = await newUser.save();
+
+  const token = jwt.sign({ id: savedUser._id }, config.SECRET, {
+    expiresIn: 86400,
+  });
+
+  res.status(200).json({ token });
+};
+
 export const signIn = async (req, res) => {
   const { email, password } = req.body;
   const userFound = await User.findOne({ email });
