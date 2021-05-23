351fc238c88181d2bd1d5030e3f825f33f469508
Joaquin Santarcangelo <joaquinsantarcangelo@gmail.com>
2021-05-23 10:46:40 -0300

    signIn controller working

--- conexa-back/src/controllers/auth.controller.js
+++ conexa-back/src/controllers/auth.controller.js
@@ -1,5 +1,27 @@
 import User from "../models/User";
+import jwt from "jsonwebtoken";
+import config from "../config";
 
 export const signIn = async (req, res) => {
-    
-}
+  const { email, password } = req.body;
+  const userFound = await User.findOne({ email });
+
+  //User does not exists
+  if (!userFound) return res.status(400).json({ message: "User not found" });
+
+  //Compares Encrypted Password
+  const matchPassword = await User.comparePassword(
+    passowrd,
+    userFound.password
+  );
+
+  //Password Doesnt Match
+  if (!matchPassword)
+    return res.status(401).json({ token: null, message: "Invalid password" });
+
+  const token = jwt.sign({ id: userFound._id }, config.SECRET, {
+    expiresIn: 86400,
+  });
+
+  res.json({ token });
+};
--- conexa-back/src/models/User.js
+++ conexa-back/src/models/User.js
@@ -22,4 +22,4 @@ userSchema.statics.comparePassword = async (password, receivedPassword) => {
   return await bcrypt.compare(password, receivedPassword);
 };
 
-export default userSchema;
+export default model("User", userSchema);
