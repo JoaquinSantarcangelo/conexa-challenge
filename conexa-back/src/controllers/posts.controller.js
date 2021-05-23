21f16606b6c0757c44ca0998efd29a9820964de2
Joaquin Santarcangelo <joaquinsantarcangelo@gmail.com>
2021-05-23 08:41:10 -0300

    Get All Posts working

--- conexa-back/src/controllers/posts.controller.js
+++ conexa-back/src/controllers/posts.controller.js
@@ -2,5 +2,8 @@ import axios from "axios";
 import { API_URL } from "../consts";
 
 export const getAllPosts = (req, res) => {
- 
+  axios
+    .get(`${API_URL}/posts`)
+    .then((data) => res.json({ posts: data.data }))
+    .catch((err) => console.log(err));
 };
