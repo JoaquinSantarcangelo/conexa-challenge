0bbb2fc99c18c4fef2b836fd8e07993f9ef65642
Joaquin Santarcangelo <joaquinsantarcangelo@gmail.com>
2021-05-24 11:49:12 -0300

    Posts component styles

--- conexa-front/src/screens/HomeScreen/PostsScreen/Post/index.js
+++ conexa-front/src/screens/HomeScreen/PostsScreen/Post/index.js
@@ -1,10 +1,23 @@
 import React from "react";
+import { Divider } from "@chakra-ui/react";
 import "./index.sass";
 
 const index = ({ data }) => {
   return (
     <div className="post">
       <div className="title">{data.title}</div>
+      <div className="body">{data.body}</div>
+      <Divider />
+      <div className="bottom-info">
+        <div className="author">Created by <span>Lorem Ipsum</span></div>
+        <div className="info-divider"></div>
+        <div className="tags">
+          <div className="text">Tags:</div>
+          <div className="tag">Lorem</div>
+          <div className="tag">Ipsum</div>
+        </div>
+        <div className="info-divider"></div>
+      </div>
     </div>
   );
 };
