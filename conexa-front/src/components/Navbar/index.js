846966c4b71de2bed2c3fdb9149d90804693b88e
Joaquin Santarcangelo <joaquinsantarcangelo@gmail.com>
2021-05-24 11:32:17 -0300

    Navbar styles

--- conexa-front/src/components/Navbar/index.js
+++ conexa-front/src/components/Navbar/index.js
@@ -1,16 +1,42 @@
 import React from "react";
 import "./index.sass";
 import { NavLink } from "react-router-dom";
+import { Button, ButtonGroup } from "@chakra-ui/react";
+import {
+  BsCollectionFill,
+  BsFillImageFill,
+  BsBoxArrowInRight,
+} from "react-icons/bs";
 
 const index = () => {
   return (
     <div className="navbar">
       <div className="wrapper-left">
-        <NavLink to="/">Posts</NavLink>
-        <NavLink to="/photos">Photos</NavLink>
+        <NavLink to="/">
+          <Button
+            mr="0.5em"
+            leftIcon={<BsCollectionFill />}
+            size="lg"
+            colorScheme="blue"
+          >
+            Posts
+          </Button>
+        </NavLink>
+        <NavLink to="/photos">
+          <Button leftIcon={<BsFillImageFill />} size="lg" colorScheme="blue">
+            Photos
+          </Button>
+        </NavLink>
       </div>
       <div className="wrapper-right">
-          <div id="logout">Logout</div>
+        <Button
+          rightIcon={<BsBoxArrowInRight />}
+          id="log-out"
+          variant="outline"
+          size="lg"
+        >
+          Log out
+        </Button>
       </div>
     </div>
   );
