1dccec121ef6337fc772fd1b19c22cefbc067826
Joaquin Santarcangelo <joaquinsantarcangelo@gmail.com>
2021-05-24 09:18:35 -0300

    Created loading component

---
+++ conexa-front/src/components/Loading/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Spinner } from "@chakra-ui/react";
+
+const index = () => {
+  return (
+    <div className="loading">
+      <Spinner
+        thickness="4px"
+        speed="0.65s"
+        emptyColor="gray.200"
+        color="blue.500"
+        size="xl"
+      />
+    </div>
+  );
+};
+
+export default index;
