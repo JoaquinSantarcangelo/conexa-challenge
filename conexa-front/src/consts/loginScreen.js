1aec268ff35ed2a65f120d613b13838b2f5973de
Joaquin Santarcangelo <joaquinsantarcangelo@gmail.com>
2021-05-24 08:47:34 -0300

    login responsive fix

--- conexa-front/src/consts/loginScreen.js
+++ conexa-front/src/consts/loginScreen.js
@@ -1,4 +1,4 @@
-export const FORM_TITLE = "Enter your crendentials";
+export const FORM_TITLE = "Log in";
 export const SUBMIT_TEXT = "Log in";
 export const INVALID_EMAIL_ERROR = "Email must be a valid email";
 export const EMPTY_EMAIL_ERROR = "Email is a required field";
