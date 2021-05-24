cc4b2e03eae257493e3a466dfa354e99beb41334
Joaquin Santarcangelo <joaquinsantarcangelo@gmail.com>
2021-05-24 05:31:52 -0300

    Photo slice typo

--- conexa-front/src/app/store.js
+++ conexa-front/src/app/store.js
@@ -2,7 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 
 //Slices
 import postSlice from "../features/post/postSlice";
-import photoSlice from "../features/post/photoSlice";
+import photoSlice from "../features/photo/photoSlice";
 import userSlice from "../features/user/userSlice";
 
 export const store = configureStore({
